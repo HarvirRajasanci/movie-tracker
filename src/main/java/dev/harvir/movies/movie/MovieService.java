ffb290724db92a8c21e8531663bc2c549bc7c8eb
Harvir R <136796141+HarvirRajasanci@users.noreply.github.com>
2025-05-19 02:16:53 -0700

    Created MovieService

--- src/main/java/dev/harvir/movies/movie/MovieService.java
+++ src/main/java/dev/harvir/movies/movie/MovieService.java
@@ -1,6 +1,7 @@
-package dev.harvir.movies;
+package dev.harvir.movies.movie;
 
-import org.bson.types.ObjectId;
+import dev.harvir.movies.movie.Movie;
+import dev.harvir.movies.movie.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
