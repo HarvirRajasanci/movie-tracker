a2a4d8181896283d86ef9229cf445daec063c85c
Harvir R <136796141+HarvirRajasanci@users.noreply.github.com>
2025-05-19 02:17:48 -0700

    Created ReviewRepository

---
+++ src/main/java/dev/harvir/movies/review/ReviewRepository.java
@@ -0,0 +1,10 @@
+package dev.harvir.movies.review;
+
+import dev.harvir.movies.review.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
