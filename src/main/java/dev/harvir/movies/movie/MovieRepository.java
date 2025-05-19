18a1e671705906a6e5b39268a896e2c337421ec0
Harvir R <136796141+HarvirRajasanci@users.noreply.github.com>
2025-05-19 02:17:19 -0700

    Created MovieRepository

--- src/main/java/dev/harvir/movies/movie/MovieRepository.java
+++ src/main/java/dev/harvir/movies/movie/MovieRepository.java
@@ -1,5 +1,6 @@
-package dev.harvir.movies;
+package dev.harvir.movies.movie;
 
+import dev.harvir.movies.movie.Movie;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
